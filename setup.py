5034d7dc7d6ac81f9a0e985a13ba3275cb56caaa
Dishit Pala <dishitpala@gmail.com>
2018-08-08 14:19:02 +0530

    stage-3

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="column",
+    name="analytic",
     version="1.0",
     author="Dishit Pala",
     author_email="dishitpala@gmail.com",
     description="A python library for fetching, cleaning & sorting the CSV file",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dishitpala/column",
+    url="https://github.com/dishitpala/analytic",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
